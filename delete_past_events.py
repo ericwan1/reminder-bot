2d2e31cd8c03e6c92bfed98c19f4a85ef28f2aae
Eric Wan <ericwan@andrew.cmu.edu>
2023-01-09 18:58:14 -0600

    1/9/23
    Changes to code due to design change

--- delete_past_events.py
+++ delete_past_events.py
@@ -5,16 +5,9 @@ client = bigquery.Client()
 table_id = "ks-reminder-bot.gs_event_data.gs-event-df"
 prod_df = client.list_rows(table_id).to_dataframe()
 
-# Want to remove all rows where the flags are 'True' which indicates the event has fired and passed
-# Until we figure out definitively on the str/varchar values for the event_time column, we will delete rows where
-# all reminders have been made; but deleting by past event times, regardless of reminders done makes more sense
+# Want to check the date for each object we have in the BigQuery. 
+# If this is not feasible with DML/SQL manipulation, then we may need to just delete/reupload. 
 dml_statement = (
-    "DELETE gs_event_data.gs-event-df"
-    "WHERE week_remind = TRUE"
-    "AND twoday_remind = TRUE"
-    "AND morningof_remind = TRUE"
-    "AND hour_remind = TRUE"
-    "AND tenmin_remind = TRUE"
 )
 query_job = client.query(dml_statement)
 query_job.result()
