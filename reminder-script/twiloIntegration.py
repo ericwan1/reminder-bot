3c8b9ba5824541b50edd8e2be4a36b4912b23bd3
Lawrence <39733044+Lawrence-O@users.noreply.github.com>
2023-03-16 00:02:09 -0400

    Updated TwiloIntegration.py
    script imports contacts from a csv file in the same directory and sends the message to all contacts there.

--- reminder-script/twiloIntegration.py
+++ reminder-script/twiloIntegration.py
@@ -1,37 +1,28 @@
-import csv
+import pandas as pd
 import os
 
 from twilio.rest import Client
 
 
-def twilo_sendmsg(message = "Test"):
+def twilo_sendmsg(message_to_send = "Hello $name$"):
     # Account SID from twilio.com/console
-    account_sid = "AC4878955fa4507e782386ab45829e7824"
-    #account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
     # Auth Token from twilio.com/console
-    auth_token  = "e69a1cd59fb765a22211d7ff6842289d"
-    #auth_token  = "your_auth_token"
+    auth_token  = "your_auth_token"
     # Host Phone Number
-    host_number = "+19382224809"
+    host_number = "+1999999999"
     numbers = []
     country_code = "+1"
     file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "users.csv")
-    with open(file_path) as f:
-        for number in f:
-            number = number.strip()
-            number = number.replace("-","")
-            print(number)
-            if number[0] == "+":
-                numbers.append(number)
-            else:
-                numbers.append(country_code+number)
+    users_df = pd.read_csv(file_path)
     client = Client(account_sid, auth_token)
-    for number in numbers:
-        print("sending messages to:", number)
+    for user,phone_number in zip(users_df["name"],users_df["phone_number"]):
+        if "$name$" in message_to_send:
+            message_to_send = message_to_send.replace("$name$", str(user))
         message = client.messages.create(
-            to=number, 
+            to=phone_number, 
             from_=host_number,
-            body=message)
-    print("done")
+            body=message_to_send)
+    
 
-twilo_sendmsg("TEST FROM TWILLO")
+# twilo_sendmsg("TEST FROM TWILLO")
